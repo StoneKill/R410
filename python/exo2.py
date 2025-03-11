56eab9e13e356d9b921954ce433dca6c8542b8f4
Yanis <degray231@unicaen.fr>
2025-03-11 14:54:48 +0100

    REFONTE DU DECAL LETTRE + REFONTE DU CHIFFRE CESAR

--- python/exo2.py
+++ python/exo2.py
@@ -1,5 +1,36 @@
-def decal_lettre(lettre, x):
-    if lettre.isalpha():
-        return chr(ord(lettre) + (x%26))
-    else:
+def decal_lettre(lettre: str, x: int):
+    # Si lettre n'est pas une lettre, on renvoit la lettre tel quel
+    if not lettre.isalpha():
         return lettre
+    
+
+    if lettre.isupper():
+        # Vu que ord(A) = 65, pour normaliser on fait - 65
+        lettre_norm = ord(lettre) - 65
+
+        # On fait le décalage, on modulo 26 pour rester dans l'alphabet, on dénormalise et on retransforme en char
+        return chr(((lettre_norm + x) % 26) + 65)
+    else:
+        # Vu que ord(A) = 65, pour normaliser on fait - 97
+        lettre_norm = ord(lettre) - 97
+        
+        # On fait le décalage, on modulo 26 pour rester dans l'alphabet, on dénormalise et on retransforme en char
+        return chr(((lettre_norm + x) % 26)+ 97) 
+
+
+
+
+def chiffre_cesar(message: str, decalage: int, chiffre: bool=True):
+    newChaine = str()
+
+    if chiffre:
+        for char in message:
+            newChaine += decal_lettre(char, decalage)
+        return newChaine
+    else:
+        for char in message:
+            newChaine += decal_lettre(char, -decalage)
+        return newChaine
+
+def bruteForce_Caesar(message):
+    pass 
